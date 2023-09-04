b2f80aed7594361f22e8cc18d46f4956d0231da4
ruslan <bumerboy86@gmail.com>
2023-09-04 18:25:04 +0600

    Changed main file

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -6,6 +6,7 @@ import store from './store/store.ts'
 import App from './App.tsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <ToastContainer theme='colored' autoClose={2000}/>
